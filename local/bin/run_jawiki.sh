d5130fae1d4b62dac32a4b8e6ffd72cad6a94c3b
tools.archiving <tools.archiving@tools-bastion-02.eqiad.wmflabs>
2015-05-05 02:14:40 +0000

    Remove translate page name as argument

--- local/bin/run_jawiki.sh
+++ local/bin/run_jawiki.sh
@@ -1,1 +1,1 @@
-~/local/bin/python /shared/pywikipedia/core/pwb.py archivebot -locale:ja_JP.utf8 -lang:ja -family:wikipedia -user:ArchiverBot 利用者:ArchiverBot/config User:ArchiverBot/config -dir:$HOME/.pywikibot -putthrottle:60
+~/local/bin/python /shared/pywikipedia/core/pwb.py archivebot -locale:ja_JP.utf8 -lang:ja -family:wikipedia -user:ArchiverBot User:ArchiverBot/config -dir:$HOME/.pywikibot -putthrottle:60 
