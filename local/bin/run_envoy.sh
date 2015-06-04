e2d94afa6bc8a370985810120e527011158656a8
Yusuke Matsubara <whym@whym.org>
2015-06-04 03:09:20 +0000

    Add Commons, add "time"

--- local/bin/run_envoy.sh
+++ local/bin/run_envoy.sh
@@ -1,2 +1,2 @@
 #! /bin/bash
-python /shared/pywikipedia/core/pwb.py archivebot -lang:en -family:wikivoyage -user:ArchiverBot -dir:$HOME/.pywikibot -putthrottle:10 -salt:goldenretriever Template:Auto_archiving
+time python /shared/pywikipedia/core/pwb.py archivebot -lang:en -family:wikivoyage -user:ArchiverBot -dir:$HOME/.pywikibot -putthrottle:10 Template:Auto_archiving
--- local/bin/run_meta.sh
+++ local/bin/run_meta.sh
@@ -1,2 +1,2 @@
 #! /bin/bash
-python /shared/pywikipedia/core/pwb.py archivebot -lang:meta -family:meta -user:ArchiverBot -dir:$HOME/.pywikibot -putthrottle:7 User:MiszaBot/config
+time python /shared/pywikipedia/core/pwb.py archivebot -lang:meta -family:meta -user:ArchiverBot -dir:$HOME/.pywikibot -putthrottle:7 User:MiszaBot/config
