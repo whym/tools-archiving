c7a49182e292dd44fd556e8c9c56894e52a56bb6
Yusuke Matsubara <whym@whym.org>
2015-08-07 09:31:28 +0000

    Shorter sleep time

--- local/bin/run_commons.sh
+++ local/bin/run_commons.sh
@@ -1,2 +1,2 @@
 #! /bin/bash
-time python $PWB archivebot -lang:commons -family:commons -user:ArchiverBot -dir:$HOME/.pywikibot -putthrottle:10 User:MiszaBot/config
+time python $PWB archivebot -lang:commons -family:commons -user:ArchiverBot -dir:$HOME/.pywikibot -putthrottle:10 User:MiszaBot/config -sleep:2
--- local/bin/run_meta.sh
+++ local/bin/run_meta.sh
@@ -1,2 +1,2 @@
 #! /bin/bash
-time python $PWB archivebot -lang:meta -family:meta -user:ArchiverBot -dir:$HOME/.pywikibot -putthrottle:7 User:MiszaBot/config
+time python $PWB archivebot -lang:meta -family:meta -user:ArchiverBot -dir:$HOME/.pywikibot -putthrottle:10 User:MiszaBot/config -sleep:2
